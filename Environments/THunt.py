26e3943925645666674eadf092b99d55cd07acbb
Evan Hurwitz <evan.hurwitz@avast.com>
2022-11-15 17:32:13 +0000

    added field size parameter to the treasure hunt initialiser

--- Environments/THunt.py
+++ Environments/THunt.py
@@ -2,15 +2,16 @@ import numpy as np
 
 class treasure_hunt:
 
-    def __init__(self, treasure_location: tuple = (2, 2)):
+    def __init__(self, field_size: tuple = (5, 5), treasure_location: tuple = (2, 2)):
         """
         creates a treasure hunt object. The treasure is hidden at the treasure location
 
         :treasure location: the location of the treasure to find
+        :field_size: tuple with the maximum value of each dimension
         """
 
         self.treasure = treasure_location
-        self.location = (1,1)
+        self.location = (1, 1)
 
     def take_step(self, action: int) -> int:
 
