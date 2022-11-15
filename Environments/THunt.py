be2919930f1c7a8b88121afe8acfc39cdfa40029
Evan Hurwitz <evan.hurwitz@avast.com>
2022-11-15 17:15:27 +0000

    added stubs for treasure hunt

--- Environments/THunt.py
+++ Environments/THunt.py
@@ -1,2 +1,26 @@
 import numpy as np
 
+class treasure_hunt:
+
+    def __init__(self, treasure_location: tuple = (2, 2)):
+        """
+        creates a treasure hunt object. The treasure is hidden at the treasure location
+
+        :treasure location: the location of the treasure to find
+        """
+
+        self.treasure = treasure_location
+        self.location = (1,1)
+
+    def take_step(self, action: int) -> int:
+
+        return None
+
+    def reset_prize(self, treasure_location: tuple) -> None:
+
+        return None
+
+    def get_current_state(self) -> int:
+
+        return None
+
