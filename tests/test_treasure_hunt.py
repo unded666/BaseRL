616e0c30f66a95d4d5d8fa1a0a4d265fcf85afa6
Evan Hurwitz <evan.hurwitz@avast.com>
2022-11-16 09:44:42 +0000

    added treasure hunt testing code stubs

---
+++ tests/test_treasure_hunt.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from Environments.THunt import treasure_hunt
+import numpy as np
+
+class TreasureTest(TestCase):
+    def SetUp(self) -> None:
+
+        pass
+
+    def test_initialisation(self) -> None:
+
+        pass
+
+    def test_take_step(self) -> None:
+
+        pass
+
+    def test_reset_prize(self) -> None:
+
+        pass
+
+    def test_get_current_state(self) -> None:
+
+        pass
+
+
